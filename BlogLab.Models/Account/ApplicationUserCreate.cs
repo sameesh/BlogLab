698a248a01aba088700c2574c720005ef7c804c5
Sameesh <sameeshkumar@gmail.com>
2021-12-15 19:35:50 +0530

    Models Project Complted. Repository Started

---
+++ BlogLab.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserCreate:ApplicationUserLogin
+    {
+        [Required(ErrorMessage = "User name required")]
+        [MinLength(10, ErrorMessage = "Must be atleast 10-20 characters")]
+        [MaxLength(30, ErrorMessage = "User name cannot exceed 30 characters")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [MinLength(30, ErrorMessage = "Can be atmost  30 characters")]
+        [EmailAddress( ErrorMessage = "InValid Email Format")]
+        public string Email { get; set; }
+    }
+}
---
+++ BlogLab.Models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserLogin
+    {
+        [Required(ErrorMessage ="User name required")]
+        [MinLength(5,ErrorMessage ="Must be atleast 5-20 characters")]
+        [MaxLength(20,ErrorMessage ="User name cannot exceed 20 characters")]
+        public string  Username { get; set; }
+
+        [Required(ErrorMessage = "Password required")]
+        [MinLength(10, ErrorMessage = "Must be atleast 10-50 characters")]
+        [MaxLength(50, ErrorMessage = "Password cannot exceed 20 characters")]
+        public string  Password { get; set; }
+
+    }
+}
